9fae62b3694fa7cdca31f0ca14f366907cf33ded
Aaron Faber <Aaron.Faber@digitaslbi.com>
2015-02-04 12:09:50 +0000

    Abstracted the junit test cases so that different scenarios can be tested more easily

--- test/junit/junit.test.js
+++ test/junit/junit.test.js
@@ -2,29 +2,41 @@
 
 var should = require('should'),
     fs = require('fs'),
-    xmlEmitter = require('../../lib/junit_emitter');
+    xmlEmitter = require('../../lib/junit_emitter'),
+    mockXMLResults,
+    xmlText;
 
-describe('junit', function () {
-    var mockXMLResults;
-    var xmlText;
+function getMock(path, done) {
+    fs.readFile(path, function (err, data) {
+        if (err) return done(err);
+        mockXMLResults = data.toString('utf8');
+        done();
+    });
+}
 
-    before(function (done) {
-        fs.readFile('./test/junit/fixtures/mock.xml', function (err, data) {
-            if (err) return done(err);
-            mockXMLResults = data.toString('utf8');
-            done();
+function getJsonErrors(jsonErrors) {
+    var errors = require(jsonErrors);
+
+    xmlText = xmlEmitter.getHeader(errors);
+    xmlText = xmlText.concat(xmlEmitter.formatContent(errors));
+    xmlText = xmlText.concat(xmlEmitter.getFooter());
+}
+
+describe.only('junit', function () {
+    describe('when there are errors in a single javascript file', function () {
+
+        before(function (done) {
+            var mockFilePath = './test/junit/fixtures/mock_one_file.xml';
+            getMock(mockFilePath, done);
         });
-    });
 
-    beforeEach(function () {
-        var errors = require('./fixtures/errors');
+        beforeEach(function () {
+            var jsonErrorsFilePath = './fixtures/errors_one_file';
+            getJsonErrors(jsonErrorsFilePath);
+        });
 
-        xmlText = xmlEmitter.getHeader(errors);
-        xmlText = xmlText.concat(xmlEmitter.formatContent(errors));
-        xmlText = xmlText.concat(xmlEmitter.getFooter());
-    });
-
-    it('should transform JSHint results in junit XML', function () {
-        xmlText.should.equal(mockXMLResults);
+        it('should transform JSHint results in junit XML', function () {
+            xmlText.should.equal(mockXMLResults);
+        });
     });
 });
